f3d33f0be9cb3e8eb4ab7512386705c922118e61
mohamadisara20 <mohamadi.sara20@gmail.com>
2021-12-31 01:50:55 +0100

    added leaf ranges for eng

--- tree.py
+++ tree.py
@@ -16,7 +16,7 @@ class Node:
         self.multinuc = multinuc
         self.leaf_range = leaf_range
         self.node_text = ''
-
+        self.word_count = 0
 
 
 def rebuild_tree_old(tree_node):
@@ -64,12 +64,13 @@ def get_index_in_sent(my_list, word):
             return index
 
 
-def xml2tree(fname):
+def xml2tree(fname, tokens, nlp):
     with open(fname) as f:
         content = f.read().replace('&lt;P&gt;', '')
 
     node_dict = dict({})
     leaf_idx = []
+    c = 0
 
     nodes = re.findall('id="\\d+.*"', content)
     leaf_nodes = re.findall('<segment id="\\d+".*', content)
@@ -78,6 +79,7 @@ def xml2tree(fname):
     for ln in leaf_nodes:
         idx = re.findall('\\d+', ln)[0]
         leaf_idx.append(idx)
+
 
     # create nodes to be filled later
     node_dict = dict({})
@@ -90,13 +92,18 @@ def xml2tree(fname):
         idx = re.findall('id="\\d+"', node)[0]
         idx = re.findall('\\d+', idx)[0]
         node_dict[idx].node_text = node
+    
+    root_ind = -1
 
     for node in nodes:
         # skip root
         if 'parent' not in node:
+            idx = re.findall('id="\\d+"', node)[0]
+            idx = re.findall('\\d+', idx)[0]
+            node_dict[idx].root = True
+            root_ind = idx
             pass
         else:
-
             # find relation name
             idx = re.findall('id="\\d+"', node)[0]
             idx = re.findall('\\d+', idx)[0]
@@ -105,19 +112,19 @@ def xml2tree(fname):
             relname = re.findall('relname="[\w+-?]+"', node)[0]
             relname = relname[9:len(relname) - 1]
             node_dict[idx].rel = relname
+            
             if idx in leaf_idx:
                 node_dict[idx].is_leaf = True
+                for ln in leaf_nodes:
+                    if 'segment id="' + idx + '"' in ln:
+                        ln_text = re.sub(r'<.*>(.*)<\/\w+>', '\\1', ln)
+                        ln_text_processed = nlp(ln_text)
+                        count_all_sents_words = 0
+                        for sent in ln_text_processed.sentences:
+                            count_all_sents_words += len(sent.words)
+                        
+                        node_dict[idx].word_count = count_all_sents_words
 
-            if idx in leaf_idx:
-                pass
-                # for ln in leaf_nodes: if 'segment id="' + idx + '"' in ln: ln_text = re.sub(r'<.*>(.*)<\/\w+>',
-                # '\\1', ln) for i in range(len(tokens.sentences)): if i > 0: begin += len(tokens.sentences[
-                # i-1].words) end += len(tokens.sentences[i-1].words) else: begin = 0 end = 0 if ln_text in
-                # tokens.sentences[i].text: ln_text_processed = nlp(ln_text) if len(ln_text_processed.sentences) ==
-                # 1: s = begin + ln_text_processed.sentences[0].words[0].id - 1 e = end +
-                # ln_text_processed.sentences[0].words[-1].id - 1 node_dict[idx].leaf_range = str(s) + " " + str(e)
-                # else: print('############### length greater than one: ' + str(len(ln_text_processed.sentences)) +
-                # "########")
 
             # find the nuclearity label
             if 'type' in node:
@@ -165,6 +172,14 @@ def xml2tree(fname):
                 print('exception')
             # Assign left and right nodes
 
+            lf_idx = sorted(leaf_idx, key=lambda x: float(x))
+
+            counter = 0
+            for lidx in lf_idx:
+                node_dict[lidx].leaf_range = str(counter) + " " + str(counter + node_dict[lidx].word_count - 1)
+                counter += node_dict[lidx].word_count
+
+
             if node_dict[parent_id].left is None:
                 node_dict[parent_id].left = node_dict[idx]
             elif idx < node_dict[parent_id].left.node_id:
@@ -174,9 +189,9 @@ def xml2tree(fname):
             else:
                 node_dict[parent_id].right = node_dict[idx]
 
-
-    node_dict['1'].root = True
-    return node_dict['1'], leaf_idx
+    gg = Node(None,None,'-10')
+    gg.left = node_dict[root_ind]
+    return gg, leaf_idx
 
 
 def load_relation_mappings(filename):
@@ -204,6 +219,7 @@ def rebuild_tree(tree_node):
                 new_node.rel = rel_dict[tree_node.right.rel]
                 new_node.multinuc = 'l'
                 tree_node.right.rel = 'span'
+
             else:
                 new_node.rel = rel_dict[tree_node.left.rel]
                 new_node.multinuc = 'r'
@@ -213,7 +229,7 @@ def rebuild_tree(tree_node):
             new_node.rel = rel_dict[tree_node.left.rel] if rel_dict[tree_node.left.rel] != 'span' else rel_dict[tree_node.rel]
             new_node.multinuc = tree_node.left.multinuc
             left_child = Node(left=None, right=None, node_id=tree_node.left.node_id, rel='leaf', is_leaf=True, multinuc='t', leaf_range=tree_node.left.leaf_range)
-            right_child = Node(left=None, right=None, node_id=tree_node.node_id, rel='leaf', is_leaf=True, multinuc='t')
+            right_child = Node(left=None, right=None, node_id=tree_node.node_id, rel='leaf', is_leaf=True, multinuc='t', leaf_range=tree_node.leaf_range)
             if tree_node.node_id < tree_node.left.node_id:
                 tree_node.left = right_child
                 tree_node.right = left_child
@@ -227,7 +243,7 @@ def rebuild_tree(tree_node):
             new_node.rel = rel_dict[tree_node.left.rel] if rel_dict[tree_node.left.rel] != 'span' else rel_dict[tree_node.rel]
             new_node.multinuc = tree_node.left.multinuc
             left_child = Node(left=None, right=None, node_id=tree_node.node_id, rel='leaf', is_leaf=True, multinuc='t', leaf_range=tree_node.leaf_range)
-            right_child = Node(left=tree_node.left.left, right=tree_node.left.right, node_id=tree_node.left.node_id, rel='span', is_leaf=tree_node.left.is_leaf, multinuc=tree_node.left.multinuc)
+            right_child = Node(left=tree_node.left.left, right=tree_node.left.right, node_id=tree_node.left.node_id, rel='span', leaf_range=tree_node.left.leaf_range, is_leaf=tree_node.left.is_leaf, multinuc=tree_node.left.multinuc)
             if tree_node.node_id < tree_node.left.node_id:
                 tree_node.left = left_child
                 tree_node.right = right_child
@@ -243,7 +259,7 @@ def rebuild_tree(tree_node):
             right_child = Node(left=None, right=None, node_id=tree_node.left.node_id, rel='leaf', is_leaf=True, multinuc='t',
                               leaf_range=tree_node.left.leaf_range)
             left_child = Node(left=tree_node.left.left.left, right=tree_node.left.left.right, node_id=tree_node.left.left.node_id,
-                               rel='span', is_leaf=tree_node.left.left.is_leaf, multinuc=tree_node.left.left.multinuc)
+                               rel='span', is_leaf=tree_node.left.left.is_leaf, multinuc=tree_node.left.left.multinuc, leaf_range= tree_node.left.left.leaf_range)
             if tree_node.left.node_id < tree_node.left.left.node_id:
                 tree_node.left = right_child
                 tree_node.right = left_child
@@ -251,12 +267,12 @@ def rebuild_tree(tree_node):
                 tree_node.left = left_child
                 tree_node.right = right_child
         
-        elif tree_node.left is not None and tree_node.left.is_leaf:
-            new_node.rel = rel_dict[tree_node.right.rel]
-            new_node.multinuc = 'c'
-            tree_node.left = tree_node.left.left
-            tree_node.right = tree_node.right.left
-            
+        # elif tree_node.left is not None and tree_node.left.is_leaf:
+        #     new_node.rel = rel_dict[tree_node.right.rel]
+        #     new_node.multinuc = 'c'
+        #     tree_node.left = tree_node.left.left
+        #     tree_node.right = tree_node.right.left
+   
     else:
         new_node.rel = rel_dict[tree_node.rel]
         new_node.multinuc = tree_node.multinuc
@@ -333,13 +349,16 @@ def pos_const_rst_builder(fname, outname):
 
 
 if __name__ == '__main__':
-    # nlp = stanza.Pipeline('en')
+    nlp = stanza.Pipeline('de')
     rel_dict = load_relation_mappings('relname_mapping.json')
     with open('wsj.txt') as f:
         text = f.read()
 
-    # tokens = nlp(text)
-    out_node, leaf_idx = xml2tree('wsj_input_rs3.xml')
+
+
+    tokens = nlp(text)
+    out_node, leaf_idx = xml2tree('maz.rs3',tokens,nlp)
     tree_rebuilt = rebuild_tree(out_node)
+    print(preorder_str(tree_rebuilt))
     # print(pos_const_rst_builder('wsj.txt', 'janc.txt'))
     
