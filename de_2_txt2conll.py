d42dc73d6cf5e061dc2f2a3d24adb6cfa0280f68
mohamadisara20 <mohamadi.sara20@gmail.com>
2022-06-14 22:07:21 +0200

    added spacy conll and text

--- de_2_txt2conll.py
+++ de_2_txt2conll.py
@@ -5,6 +5,7 @@ import re
 import traceback
 import stanza
 from stanza.utils.conll import CoNLL
+import spacy
 
 def txt2conll(nlp, file_path):
     print('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>' + file_path)
@@ -30,17 +31,58 @@ def txt2conll(nlp, file_path):
             f.write(row+'\n')
 
 
-def find_partial_parse(text, begin, word):
-    index = text.find(word, begin)
-    if (index < 0):
-        return -1
-    index += 1
-    while(index < len(text)):
-        if (text[index] == '('):
-            break
-        index += 1
-    return index
+def txt2conll_nltk(nlp, file_path):
+    print('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>' + file_path)
+    with open(f'{file_path}.txt') as f:
+        text = f.readlines()
+        
+    newConll = []
+    for sent in text:
+        doc = nlp(sent)
+        conll = CoNLL.doc2conll(doc)
+        for s in conll:
+            for token in s:
+                newConll.append(token)
+            # token = str(sent_ind) + '\t' + token
+            # newConll.append(token)
+        # sent_ind += 1
+        newConll.append('\n')
+    
+    conllCombinedPPs = combinePPs('\n'.join(newConll))
+    
+    with open(f'{file_path}.conll', 'w') as f :
+        for row in conllCombinedPPs:
+            f.write(row+'\n')
 
+def txt2spacy(nlp, file_path):
+    print('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>' + file_path)
+    with open(f'{file_path}.txt') as f:
+        text = f.read()
+    text = re.sub('\n', ' ', text)
+    text = re.sub('\s+', ' ', text)
+    
+    with open(f'{file_path}.txt_spacy', 'w') as f:
+        f.write(text)
+    
+    newConll = []
+    
+    os.system(f'parse-as-conll de_dep_news_trf spacy --input_str "{text}" > {file_path}.conll_spacy')
+    # for sent in text:
+    #     doc = nlp(sent)
+    #     conll = CoNLL.doc2conll(doc)
+    #     for s in conll:
+    #         for token in s:
+    #             newConll.append(token)
+    #         # token = str(sent_ind) + '\t' + token
+    #         # newConll.append(token)
+    #     # sent_ind += 1
+    #     newConll.append('\n')
+    
+    # conllCombinedPPs = combinePPs('\n'.join(newConll))
+    
+    # with open(f'{file_path}.conll_spacy', 'w') as f :
+    #     for row in conllCombinedPPs:
+    #         f.write(row+'\n')
 
 def combinePPs(conll):
     tokens = conll.split('\n')
@@ -66,25 +108,25 @@ def combinePPs(conll):
 
 if __name__ == "__main__":
     print('====================== text 2 conll =================')
-    if len(sys.argv) > 1:
-        data_dir = sys.argv[1]
-        nlp = stanza.Pipeline('de')
-        if data_dir.endswith('/'):
-            path = data_dir[:len(data_dir)-1]
-        all_files = os.listdir(path)
-        print(all_files)
-        counter = 0
-        for filename in sorted(all_files):
-            if not filename.endswith('.txt'):
-                continue
-            filename = filename.split('.txt')[0]
-            try:
-                txt2conll(nlp, f'{path}/{filename}')
-            except Exception as ex:
-                print('###################################################')
-                print(filename)
-                print(ex)
-                traceback.print_exc()
+    # if len(sys.argv) > 1:
+    data_dir = 'pcc2/'
+    nlp = stanza.Pipeline('de')
+    spacy_nlp = spacy.load("de_dep_news_trf")
+
+    all_files = os.listdir(data_dir)
+    counter = 0
+    for filename in sorted(all_files):
+        if not filename.endswith('.txt'):
+            continue
+        filename = filename.split('.txt')[0]
+        try:
+            # txt2conll(nlp, f'{data_dir}/{filename}')
+            txt2spacy(nlp, f'{data_dir}/{filename}')
+        except Exception as ex:
+            print('###################################################')
+            print(filename)
+            print(ex)
+            traceback.print_exc()
     else:
         print('Please specify a path to txt files!!')
 
